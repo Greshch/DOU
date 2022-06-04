58233c7bfc73f73ff2968682398c5209c8f641f7
Greshch <greshchook@gmail.com>
2022-06-04 13:16:05 +0300

    get_order() had been worked

--- stepik/main.cpp
+++ stepik/main.cpp
@@ -1,62 +1,31 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
-class Expression {
-public:
-	virtual double evaluate() = 0;
-	virtual ~Expression() {}
-};
-
-class Number : public Expression {
-private:
-	double val_;
-
-public:
-	Number(double val) : val_(val) {}
-	virtual double evaluate() override { return val_; }
-};
-
-class BinaryOperation : public Expression {
-private:
-	Expression* left_;
-	Expression* right_;
-	char type_oper_;
-
-public:
-	BinaryOperation(Expression* left, char type, Expression* right) : left_(left),
-		type_oper_(type),
-		right_(right) {}
-
-	virtual double evaluate() override {
-		switch (type_oper_) {
-		case '+' :
-			return left_->evaluate() + right_->evaluate();
-
-		case '-':
-			return left_->evaluate() - right_->evaluate();
-
-		case '*':
-			return left_->evaluate() * right_->evaluate();
-
-		case '/':
-			return left_->evaluate() / right_->evaluate();
+int get_order(char const* str) {
+	int n = 0;
+	while (*str)
+	{
+		if (*str >= '0' && *str <= '9') {
+			++n;
 		}
+		else {
+			return n;
+		}
+		++str;
 	}
-	
-	~BinaryOperation() { delete left_; delete right_; }
-};
+	return n;
+}
+
+int my_atoi(char const* str) {
+	return get_order(str);
+}
 
 int main() {
-	// сначала создаём объекты для подвыражения 4.5 * 5
-	Expression* sube = new BinaryOperation(new Number(4.5), '*', new Number(5));
-	// потом используем его в выражении для +
-	Expression* expr = new BinaryOperation(new Number(3.0), '+', sube);
-
-	// вычисляем и выводим результат: 25.5
-	std::cout << expr->evaluate() << std::endl;
-
-	// тут освобождаются *все* выделенные объекты
-	// (например, sube будет правым операндом expr, поэтому его удалять не нужно)
-	delete expr;
+	string str;
+	while (cin >> str) {
+		cout << get_order(str.c_str()) << endl;
+	}
 	return 0;
 }
